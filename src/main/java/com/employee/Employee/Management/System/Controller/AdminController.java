2407dfa0463f39f8a6e3edac011c62e3fb65c56b
mansibansal9648 <mansibansal12345@gmail.com>
2024-09-08 16:28:32 +0530

    basic validation in admin api

--- src/main/java/com/employee/Employee/Management/System/Controller/AdminController.java
+++ src/main/java/com/employee/Employee/Management/System/Controller/AdminController.java
@@ -4,6 +4,7 @@ import com.employee.Employee.Management.System.CustomResponse.ApiResponseSuccess
 import com.employee.Employee.Management.System.Dto.AdminRequest;
 import com.employee.Employee.Management.System.Entity.Admin;
 import com.employee.Employee.Management.System.Service.AdminService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +21,7 @@ public class AdminController {
     private AdminService adminService;
 
     @PostMapping("/admin/create-admin")
-    public ResponseEntity<ApiResponseSuccess<Map<String, Object>>> createAdmin(@RequestBody AdminRequest adminRequest){
+    public ResponseEntity<ApiResponseSuccess<Map<String, Object>>> createAdmin(@RequestBody @Valid AdminRequest adminRequest){
        Admin createdAdmin= adminService.createNewAdmin(adminRequest);
         ApiResponseSuccess<Map<String, Object>> response = new ApiResponseSuccess<>(new HashMap<>(), true, HttpStatus.CREATED.value(),"Admin created successfully");
         return new ResponseEntity<>(response, HttpStatus.CREATED);
--- src/main/java/com/employee/Employee/Management/System/Dto/AdminRequest.java
+++ src/main/java/com/employee/Employee/Management/System/Dto/AdminRequest.java
@@ -1,5 +1,8 @@
 package com.employee.Employee.Management.System.Dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,9 +11,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdminRequest {
+
+    @NotEmpty(message = "Name is required field")
+    @Pattern(regexp = "^[a-zA-Z\\s]{3,25}$",message = "Invalid name format")
     private String name;
+
+    @NotEmpty(message = "Email is required field")
+    @Email(message = "Invalid email address")
     private String email;
+
+    @NotEmpty(message = "User type is required field")
     private String userType;
+
+    @NotEmpty(message = "Password is required field")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,30}$", message = "Password must contain at least one lowercase letter, one uppercase letter, one number, one special character and be at least 6 characters long")
     private String password;
 
 }
